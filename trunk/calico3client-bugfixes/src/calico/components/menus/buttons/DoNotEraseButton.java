cc5d581feefd1d48bc1748b9c889c7eebc352d82
Nicolas Mangano <nmangano@ics.uci.edu>
2012-01-19 21:45:25 -0800

    * Committing rest of new palette files

---
+++ trunk/calico3client-bugfixes/src/calico/components/menus/buttons/DoNotEraseButton.java
@@ -0,0 +1,69 @@
+package calico.components.menus.buttons;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+import java.util.*;
+
+import calico.*;
+import calico.components.*;
+import calico.components.grid.*;
+import calico.components.menus.CanvasMenuButton;
+import calico.controllers.CCanvasController;
+import calico.iconsets.CalicoIconManager;
+import calico.modules.*;
+import calico.networking.*;
+import calico.networking.netstuff.CalicoPacket;
+import calico.networking.netstuff.NetworkCommand;
+
+import edu.umd.cs.piccolo.*;
+import edu.umd.cs.piccolo.util.*;
+import edu.umd.cs.piccolo.nodes.*;
+import edu.umd.cs.piccolox.nodes.PLine;
+import edu.umd.cs.piccolox.pswing.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.*;
+
+import javax.swing.JOptionPane;
+import javax.imageio.ImageIO;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
+
+import edu.umd.cs.piccolo.event.*;
+
+
+
+public class DoNotEraseButton extends CanvasMenuButton
+{
+	private static final long serialVersionUID = 1L;
+	
+	private long cuid = 0L;
+	
+	public DoNotEraseButton(long c)
+	{
+		super();
+		cuid = c;
+		try
+		{
+			setImage(CalicoIconManager.getIconImage("grid.canvas.lock"));
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void actionMouseClicked()
+	{
+		long time = (new Date()).getTime();
+		CCanvasController.lock_canvas(cuid, true, CalicoDataStore.Username, time);
+		CCanvasController.canvasdb.get(cuid).drawMenuBars();
+	}
+
+	
+}
