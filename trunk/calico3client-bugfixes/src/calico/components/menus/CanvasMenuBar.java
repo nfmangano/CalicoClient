0a289329b504d9b6e4f5fb16db51e87b25d563cd
Byron Hawkins <byron@hawkinssoftware.net>
2012-06-11 20:14:22 -0700

    Removing the "clear canvas" button until we decide exactly what it should do.

--- trunk/calico3client-bugfixes/src/calico/components/menus/CanvasMenuBar.java
+++ trunk/calico3client-bugfixes/src/calico/components/menus/CanvasMenuBar.java
@@ -100,7 +100,7 @@ public class CanvasMenuBar extends CanvasGenericMenuBar
 		//addCap();
 //		addIcon(new DoNotEraseButton(cuid));
 //		addIcon(new CanEraseButton(cuid));
-		addIcon(new ClearButton(cuid));
+//		addIcon(new ClearButton(cuid));
 		addSpacer();
 		
 //		setLock_button_array_index = text_button_array_index++;
