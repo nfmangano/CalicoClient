9421394858978f52c7502e81a4fac0103298fa09
U-nfmangano-PC\nfmangano <nfmangano@nfmangano-PC.(none)>
2012-07-16 23:57:54 -0700

    Commiting other files because git is a stubborn donkey

---
+++ updateAnalysisPluginDistInCalicoCore.sh
@@ -0,0 +1,2 @@
+cd plugins/calico.client.analysis && ant clean && ant dist && cd ../.. &&
+cp plugins/calico.client.analysis/dist/calico.client.analysis-trunk/calico.client.analysis.jar trunk/calico3client-bugfixes/plugins/calico.client.analysis.jar
---
+++ updateCalicoDistInAnalysisPlugin.sh
@@ -0,0 +1,2 @@
+cd trunk/calico3client-bugfixes && ant clean && ant dist && cd ../.. &&
+cp trunk/calico3client-bugfixes/dist/calico3client-trunk/calico3client.jar plugins/calico.client.analysis/lib/calico3client.jar
