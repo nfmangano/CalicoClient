466cfa0fe8532fd70d230b80245a37dd7c64b672
nfmangano <nfmangano@gmail.com>
2013-03-29 02:21:57 -0700

    Addresses #175, phase arrows now come from canvases, not the button.

--- plugins/IntentionalInterfacesPluginClient/src/calico/plugins/iip/components/piemenu/iip/CreateLinkButton.java
+++ plugins/IntentionalInterfacesPluginClient/src/calico/plugins/iip/components/piemenu/iip/CreateLinkButton.java
@@ -1,8 +1,11 @@
 package calico.plugins.iip.components.piemenu.iip;
 
+import java.awt.Point;
+
 import calico.components.piemenu.PieMenuButton;
 import calico.inputhandlers.InputEventInfo;
 import calico.plugins.iip.components.CCanvasLink;
+import calico.plugins.iip.components.CIntentionCell;
 import calico.plugins.iip.controllers.CIntentionCellController;
 import calico.plugins.iip.iconsets.CalicoIconManager;
 import calico.plugins.iip.inputhandlers.CIntentionCellInputHandler;
@@ -25,8 +28,15 @@ public class CreateLinkButton extends PieMenuButton
 	{
 		super.onPressed(event);
 
-		CreateIntentionArrowPhase.INSTANCE.startCreate(
-				CIntentionCellController.getInstance().getCellById(CIntentionCellInputHandler.getInstance().getActiveCell()), event.getGlobalPoint(),
-				CreateIntentionArrowPhase.NewLinkMode.LINK_EXISTING);
+		CIntentionCell cell = CIntentionCellController.getInstance().getCellById(CIntentionCellInputHandler.getInstance().getActiveCell());
+		
+		if (cell != null)
+		{
+
+			Point rootPoint = new Point((int)cell.getGlobalBounds().getCenterX(), (int)cell.getGlobalBounds().getCenterY());
+			CreateIntentionArrowPhase.INSTANCE.startCreate(
+					CIntentionCellController.getInstance().getCellById(CIntentionCellInputHandler.getInstance().getActiveCell()), rootPoint,
+					CreateIntentionArrowPhase.NewLinkMode.LINK_EXISTING);
+		}
 	}
 }
